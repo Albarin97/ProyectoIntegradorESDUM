d1ecf0d8128c2669617a41f33e2f527a883cb31b
Albarin97 <albar00@hotmail.com>
2023-10-18 15:26:41 -0600

    Funcionan botones en Product

--- Js/index.js
+++ Js/index.js
@@ -6,40 +6,58 @@ const cart = document.getElementById('cart');
 const product = document.getElementById('product');
 const newUser = document.getElementById('newUser');
 
+var iframe = document.getElementById('main-item');
+
 const main = document.getElementById('main-item');
-console.log(main);
+
 
 principal.addEventListener('click', () => {
-    console.log(main);
     main.src = "views/paginaProducto.html";
 });
 
 nosotros.addEventListener('click', () => {
-    console.log(main);
     main.src = "views/sobreNosotros.html";
 });
 
 contact.addEventListener('click', () => {
-    console.log(main);
     main.src = "views/contact.html";
 });
 
 wish.addEventListener('click', () => {
-    console.log(main);
     main.src = "views/listaDeseos.html";
 });
 
 cart.addEventListener('click', () => {
-    console.log(main);
     main.src = "views/carritoCompra.html";
 });
 
 product.addEventListener('click', () => {
-    console.log(main);
     main.src = "views/product.html";
 });
 
 newUser.addEventListener('click', () => {
-    console.log(main);
     main.src = "views/registro.html";
 });
+
+
+iframe.onload = function() {
+    var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    var elementInsideIframe = iframeDocument.getElementById('buy-now');
+    var elementInsideIframe2 = iframeDocument.getElementById('add_wishL');
+    
+
+    // Ahora puedes trabajar con el elemento dentro del iframe
+    if (elementInsideIframe) {
+        // Realiza las operaciones que necesitas
+        elementInsideIframe.addEventListener('click', () => {
+            main.src = "views/carritoCompra.html";
+        });
+    }
+    if (elementInsideIframe2) {
+        elementInsideIframe2.addEventListener('click', () => {
+            main.src = "views/listaDeseos.html";
+        });
+    }
+};
+
+
