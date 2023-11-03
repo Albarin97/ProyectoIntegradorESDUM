3fd7f32b05a2678d943d28a7798d7a48f9cee01d
CeciCanedo <ceciliacanedolozada@gmail.com>
2023-11-03 10:25:12 -0600

    Navbar editado para acceso a sesion iniciada o iniciar sesion

--- Js/estadoSesion.js
+++ Js/estadoSesion.js
@@ -30,4 +30,74 @@ document.addEventListener("DOMContentLoaded", function(){
             window.location.href="../views/inicioSesion.html";
         }
     })
-})
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    const btnCorazon= document.getElementById("btnCorazon");
+
+    btnCorazon.addEventListener("click", function(){
+        const sesionIniciada= localStorage.getItem("sesionIniciada");
+
+        if(sesionIniciada==="true"){
+            window.location.href= "../views/listaDeseos.html";
+        }else{
+            window.location.href="../views/inicioSesion.html";
+        }
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    const btnCarrito= document.getElementById("btnCarrito");
+
+    btnCarrito.addEventListener("click", function(){
+        const sesionIniciada= localStorage.getItem("sesionIniciada");
+
+        if(sesionIniciada==="true"){
+            window.location.href= "../carritoCompra.html";
+        }else{
+            window.location.href="../views/inicioSesion.html";
+        }
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    const btnPerfil= document.getElementById("btnPerfil");
+
+    btnPerfil.addEventListener("click", function(){
+        const sesionIniciada= localStorage.getItem("sesionIniciada");
+
+        if(sesionIniciada==="true"){
+            window.location.href= "../views/paginaUsuario.html";
+        }else{
+            window.location.href="../views/inicioSesion.html";
+        }
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    const btnDeseos= document.getElementById("btnDeseos");
+
+    btnDeseos.addEventListener("click", function(){
+        const sesionIniciada= localStorage.getItem("sesionIniciada");
+
+        if(sesionIniciada==="true"){
+            window.location.href= "../views/listaDeseos.html";
+        }else{
+            window.location.href="../views/inicioSesion.html";
+        }
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    const btnCompras= document.getElementById("btnCompras");
+
+    btnCompras.addEventListener("click", function(){
+        const sesionIniciada= localStorage.getItem("sesionIniciada");
+
+        if(sesionIniciada==="true"){
+            window.location.href= "../carritoCompra.html";
+        }else{
+            window.location.href="../views/inicioSesion.html";
+        }
+    })
+});
